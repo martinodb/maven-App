7255e4c0a6e8bf2ce38063146a62933f5d368198
martin <martinobal@gmail.com>
2017-03-29 19:28:04 +0200

    add code to test lang3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,5 +10,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        if(ArrayUtils.contains(args, "-snn")){
+			System.out.println("it does.");
+			} else {
+				System.out.println("it does not.");
+				}
     }
 }
